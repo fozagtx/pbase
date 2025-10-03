1b6a8392f05413190ed50d91d3f5857dc58767eb
devpima <ibrahimpima76@gmail.com>
2025-10-03 20:07:04 +0000

    Fix: Add missing React imports in main.tsx

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { DAppKitProvider } from '@vechain/dapp-kit-react';
 import { ChakraProvider } from '@chakra-ui/react';
-import {THOR_URL} from "./config/constants.ts";
+import { THOR_URL } from "./config/constants.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
